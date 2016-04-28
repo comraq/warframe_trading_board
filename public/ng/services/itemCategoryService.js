bddff9162f73f79192a0cf1810e31ec4ebb6cee9
comraq <yinadam@hotmail.com>
2016-04-28 07:19:58 -0700

    refactoring angular services as well as implementing caching to avoid excess server queries

--- public/ng/services/itemCategoryService.js
+++ public/ng/services/itemCategoryService.js
@@ -1,30 +1,51 @@
 module.exports = [ "$http", "$q", function($http, $q) {
-  this.getCategory = function getCategory(path) {
-    return $http.get("/api/category" + path)
-      .then(function success(res) {
-        // API returns ItemCategory Object
-        return res.data.category;
+  var cache = {};
 
-      }, function error(err) {
-        // Promise rejected, return null
-        return null;
-      });
-  };
+  return {
+    getCategory: function getCategory(path) {
+      if (cache[path] && cache[path].category !== undefined)
+        return cache[path].category;
 
-  this.getChildren = function getChildren(path) {
-    return $http.get("/api/category/children" + path)
-      .then(function success(res) {
-        if (!res.data.children || res.data.children.length == 0)
+      return $http.get("/api/category" + path)
+        .then(function success(res) {
+          // API returns ItemCategory Object
+          if (!cache[path])
+            cache[path] = {};
+
+          cache[path].category = res.data.category;
+          return res.data.category;
+  
+        }, function error(err) {
+          // Promise rejected, return null
           return null;
-
-        // API returns array of children category names
-        return res.data.children.map(function(e, i, arr) {
-          return e.name;
         });
+    },
 
-      }, function error(err) {
-        // Promise rejected, return null
-        return null;
-      });
+    getChildren: function getChildren(path) {
+      if (cache[path] && cache[path].children !== undefined)
+        return cache[path].children;
+
+      return $http.get("/api/category/children" + path)
+        .then(function success(res) {
+          if (!cache[path])
+            cache[path] = {};
+
+          if (!res.data.children || res.data.children.length == 0) {
+            cache[path].children = null;
+            return null;
+          }
+  
+          // API returns array of children category names
+          cache[path].children = res.data.children.map(function(e) {
+            return e.name;
+          });
+          return cache[path].children;
+  
+        }, function error(err) {
+          // Promise rejected, return null
+          return null;
+        });
+    }
   };
+
 }];
